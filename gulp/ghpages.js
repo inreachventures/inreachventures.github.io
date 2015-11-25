d43fd621e49e1cb3dd19fbcc211be172df3d0a0a
Ben Smith <ben@thesmith.co.uk>
2015-11-25 16:43:36 +0000

    hopefully deploy to staging repo

--- gulp/ghpages.js
+++ gulp/ghpages.js
@@ -5,7 +5,8 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
   gulp.task('ghpages', function(done) {
     return gulp.src(taskTarget + '/**/*')
       .pipe(ghPages({
-        branch: "master"
+        branch: "master",
+        remoteUrl: "git@github.com:inreachventures/staging-inreachventures.github.io.git"
       }));
   });
 };
