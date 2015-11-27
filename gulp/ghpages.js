da3778fc3aea3320ea923b909f59a56b2a7678ff
Ben Smith <ben@thesmith.co.uk>
2015-11-27 11:27:30 +0000

    give up on staging deploys. just copy over manually

--- gulp/ghpages.js
+++ gulp/ghpages.js
@@ -5,8 +5,7 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
   gulp.task('ghpages', function(done) {
     return gulp.src(taskTarget + '/**/*')
       .pipe(ghPages({
-        branch: "master",
-        remoteUrl: "git@github.com:inreachventures/staging-inreachventures.github.io.git"
+        branch: "master"
       }));
   });
 };
